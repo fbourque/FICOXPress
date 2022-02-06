fe876d631c97f89d96d9087fea3c4c1b87d2737e
Francis Bourque <fbourque.us@gmail.com>
2022-02-06 00:15:47 -0800

    fixes
    Signed-off-by: Francis Bourque <fbourque.us@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM ubuntu:18.04
 RUN apt-get update && apt-get install -y software-properties-common gcc && \
     add-apt-repository -y ppa:deadsnakes/ppa
 RUN apt-get update && apt-get install -y python3.6 python3-distutils python3-pip python3-apt
-RUN pip3 install xpress=8.13.3
+RUN pip3 install xpress==8.13.3
+COPY test.py .
+CMD ./test.py
 
 #FROM alpine:latest
 
---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import xpress as xp
+import random
+
+def xpress_test(num_vars: int, num_constraints: int) -> None:
+    random.seed(0)
+
+    problem = xp.problem()
+    binvars = [xp.var(vartype=xp.binary) for _i in range(num_vars)]
+    problem.addVariable(binvars)
+
+    for _i in range(num_constraints):
+        problem.addConstraint(xp.Sum((2*random.random()-1) * binvar for binvar in binvars) <= 2*random.random() - 1)
+
+    problem.setObjective(xp.Sum(xp.Sum((2*random.random()-1) * binvar for binvar in binvars)))
+    problem.solve()
+
+if __name__ == "__main__":
+    xpress_test(num_vars = 1000, num_constraints = 50)
